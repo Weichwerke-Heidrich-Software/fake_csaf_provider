eebf21130abef15a4db9fad9d118ecea09716c94
Simon Heidrich <98020056+SimonHeidrich@users.noreply.github.com>
2025-11-30 11:26:40 +0100

    Security txt improvements. (#8)

--- fake_csaf_provider/util.py
+++ fake_csaf_provider/util.py
@@ -7,5 +7,28 @@ def now():
     return datetime.datetime.now(datetime.timezone.utc).replace(microsecond=0).isoformat()
 
 
-def security_txt_content():
-    return f"CSAF: http://{domain}/obscure/path/to/provider-metadata.json\n"
+def security_txt_content(canonical_path: str):
+    expires = (datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(days=30)).replace(microsecond=0).isoformat()
+    # Most of this is just example content.
+    # Only the URLs behind Canonical and CSAF are supported.
+    return f"""
+# Our canonical URI.
+Canonical: https://{domain}{canonical_path}
+
+# Our security addresses.
+Contact: mailto:info@example.com
+
+# Our security acknowledgements page.
+Acknowledgments: https://{domain}/acknowledgments
+
+# Our preferred languages.
+Preferred-Languages: en, de
+
+# Our security policy.
+Policy: https://{domain}/policy
+
+# Our security advisories
+CSAF: http://{domain}/obscure/path/to/provider-metadata.json
+
+Expires: {expires}
+"""
