5783dc172d05c61c0af9293fbc41c8742e82d98a
Simon Heidrich <info@wwh-soft.com>
2026-03-25 17:27:24 +0100

    Conditionally offering OpenPGP key.

--- fake_csaf_provider/util.py
+++ fake_csaf_provider/util.py
@@ -1,5 +1,6 @@
 import datetime
 import os
+from .state import get_config
 
 domain = os.environ.get("FAKE_CSAF_DOMAIN", "localhost")
 port = os.environ.get("FAKE_CSAF_PORT", 34443)
@@ -12,8 +13,13 @@ def now():
 
 def security_txt_content(canonical_path: str):
     expires = (datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(days=30)).replace(microsecond=0).isoformat()
+    
+    openpgp_line = ""
+    if get_config('openpgp'):
+        openpgp_line = f"\n# Our OpenPGP key\nEncryption: https://{domain_print}/.well-known/openpgpkey.asc\n"
+    
     # Most of this is just example content.
-    # Only the URLs behind Canonical and CSAF are supported.
+    # Only the URLs behind Canonical, CSAF and Encryption are supported.
     return f"""
 # Our canonical URI.
 Canonical: https://{domain_print}{canonical_path}
@@ -32,6 +38,6 @@ Policy: https://{domain_print}/policy
 
 # Our security advisories
 CSAF: https://{domain_print}/obscure/path/to/provider-metadata.json
-
+{openpgp_line}
 Expires: {expires}
 """
